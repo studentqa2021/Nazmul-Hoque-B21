20de6c9b1af63dba4ab0059fa28c5a989408572d
studentqa2021 <sarowerhome@gmail.com>
2022-01-23 23:57:07 -0500

    login related webelement method implemented

--- src/main/java/com/generic/BaseLogin.java
+++ src/main/java/com/generic/BaseLogin.java
@@ -10,5 +10,12 @@ public class BaseLogin {
 		System.setProperty("webdriver.chrome.driver", BaseConfig.getValue("DRIVER_PATH"));
 		WebDriver driver = new ChromeDriver();
 		driver.get(BaseConfig.getValue("BASEURL"));
+		
+		OrbitzPageFactory pgFactory = new OrbitzPageFactory(driver);
+		pgFactory.getHomeSignInBtn().click();
+		pgFactory.getPopupSignInBtn().click();
+		pgFactory.getEmail().sendKeys(BaseConfig.getValue("EMAIL"));
+		pgFactory.getPassword().sendKeys("PASSWORD");
+		pgFactory.getLoginSignInBtn().click();
 	}
 }
