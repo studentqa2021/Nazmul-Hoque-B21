f1b9405a6bf9e58941af15f60a774337998a24d0
studentqa2021 <sarowerhome@gmail.com>
2022-01-23 23:54:50 -0500

    created login related xpath in pagefactory

--- src/main/java/com/generic/OrbitzPageFactory.java
+++ src/main/java/com/generic/OrbitzPageFactory.java
@@ -1,5 +1,47 @@
 package com.generic;
 
-public class OrbitzPageFactory {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class OrbitzPageFactory {
+	public OrbitzPageFactory (WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy (xpath = "(//button[@type='button'])[3]")
+	private WebElement homeSignInBtn;
+	
+	@FindBy (xpath = "//a[@data-stid='link-header-account-signin']")
+	private WebElement popupSignInBtn;
+	
+	@FindBy (xpath = "//input[@id='loginFormEmailInput']")
+	private WebElement email;
+	
+	@FindBy (xpath = "//input[@id='loginFormPasswordInput']")
+	private WebElement password;
+	
+	@FindBy (xpath = "//button[@id='loginFormSubmitButton']")
+	private WebElement loginSignInBtn;
+
+	public WebElement getHomeSignInBtn() {
+		return homeSignInBtn;
+	}
+
+	public WebElement getPopupSignInBtn() {
+		return popupSignInBtn;
+	}
+
+	public WebElement getEmail() {
+		return email;
+	}
+
+	public WebElement getPassword() {
+		return password;
+	}
+
+	public WebElement getLoginSignInBtn() {
+		return loginSignInBtn;
+	}
 }
